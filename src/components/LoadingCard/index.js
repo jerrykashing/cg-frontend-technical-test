a699c7b74dd68181b462dff16fdf0f4719421773
Jerry Lee <jerry.app.dev@gmail.com>
2022-07-06 09:16:59 +0100

    update loading card style support responsive

--- src/components/LoadingCard/index.js
+++ src/components/LoadingCard/index.js
@@ -5,7 +5,7 @@ export default function LoadingCard() {
   return (
     <div className="loadingCard is-loading">
       <div className="loadingCard__image" />
-      <div className="loadingCard__content">
+      <div className="loadingCard__info">
         <div className="loadingCard__price" />
         <p />
       </div>
